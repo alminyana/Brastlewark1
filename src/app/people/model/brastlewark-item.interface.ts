783cbd77441f3f510f94e5cb503a826bfcd73c62
ENRIC ALMIÑANA CALDERON <enric.alminana@altran.es>
2020-06-21 16:26:08 +0200

    UPDATE card visualization

---
+++ src/app/people/model/brastlewark-item.interface.ts
@@ -0,0 +1,11 @@
+export interface BrastlewarkItem {
+    age: number;
+    friends: string[];
+    hair_color: string;
+    height: number;
+    id: number;
+    name: string;
+    professions: string[];
+    thumbnail: string;
+    weight: number;
+  }
